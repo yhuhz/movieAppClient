9c147f29c7ead23f87bb52f102737b268d46e593
julius.albert.grajo <julius.albert.grajo@gmail.com>
2025-05-13 10:01:44 +0800

    Remove console

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -41,7 +41,6 @@ export default function Register() {
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         if (data.message === 'Registered Successfully') {
           setEmail('');
           setPassword('');
